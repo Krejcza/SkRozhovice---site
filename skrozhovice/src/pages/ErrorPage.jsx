0e3611687e2cb50883c286fd8d96855fe0c4803e
Krejcza <155578815+Krejcza@users.noreply.github.com>
2024-09-22 21:36:10 +0200

    přidán error page

--- skrozhovice/src/pages/ErrorPage.jsx
+++ skrozhovice/src/pages/ErrorPage.jsx
@@ -1,14 +1,22 @@
+import { div } from 'framer-motion/client'
 import Transition from '../Transition'
-
-import React from 'react';
+import BeePic from '../components/images/beehive.png'
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>404 - Stránka nenalezena</h1>
-      <p>Omlouváme se, ale stránka, kterou hledáte, neexistuje.</p>
+    <div className='background-linear-deff mappp'>
+      <div className='error-pager'>
+        <div>
+          <h1>404 - Stránka nenalezena</h1>
+          <p className='sorry-error'>Omlouváme se, ale stránka, kterou hledáte, neexistuje.</p>
+        </div>
+        <div>
+          <img src={BeePic} className='image-hive' alt="" />
+          <p className='sorry-hive'>Zalezl jsi do špatného úlu!</p>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default ErrorPage;
+export default  Transition(ErrorPage);
